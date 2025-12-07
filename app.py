41b98784094dc28b812e66ba26e68880a6d6efb2
Ismail Taha <eng.isma.j.taha@gmail.com>
2025-12-07 11:19:05 +0200

    fix gunicorn app visibility

--- app.py
+++ app.py
@@ -133,14 +133,14 @@ def create_app():
 
     return app
 
-if __name__ == "__main__":
-    app = create_app()
+app = create_app()
 
+if __name__ == "__main__":
     # Create database file if not exists
     if not os.path.exists("blog.db"):
         with app.app_context():
             db.create_all()
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+    app.run(debug=True)
+
 
